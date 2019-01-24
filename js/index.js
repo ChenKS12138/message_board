df64dc1ae6e84663bb7b7ac7508c90b6f10aed05
ChenKS <42082890+ChenKS12138@users.noreply.github.com>
2019-01-24 16:59:47 +0800

    'status'

--- js/index.js
+++ js/index.js
@@ -1,25 +1,28 @@
+let serverSidePath="http://localhost/message_board/serverSide/messages.php";
 Vue.component('messages',{
     props:['item'],
-    template:'<div class="message shadow"><div class="message-nickname">{{item.nickname}}</div><div class="message-content">{{item.content}}</div><div class="message-time">{{item.time}}</div></div>',
+    template:'<div data-scroll-reveal="enter bottom" class="message shadow"><div class="message-nickname">{{item.nickname}}</div><div class="message-content">{{item.content}}</div><div class="message-time">{{item.time}}</div></div>',
 })
 let message=new Vue({
     el:'#messages',
     data:{
-        messagesContent:[
-            {nickname:'chen',content:'haha',time:'longlongago'},
-        ],
+        messagesContent:null,
+        seen:true,
     },
     created:function(){
         let response;
         let status;
-        $.get('http://localhost/message_board/serverSide/messages.php',function(data,sta){
+        $.get(serverSidePath,function(data,sta){
             response=data;
             status=sta;
         })
         let messageT=setInterval(function(){
             if(status==='success'){
                 response=JSON.parse(response);
-                message.messagesContent=response.data.messages;
+                message.messagesContent=response.data.messages.reverse();
+                $('#tip').animate({fontSize:'0px',padding:'0px'},300,function(){
+                    message.seen=false;
+                });
                 clearInterval(messageT);
             }
         },100)
@@ -30,15 +33,32 @@ let createArea=new Vue({
     data:{
         nickname:null,
         content:null,
+        buttonText:"发送",
     },
     methods: {
         update:function(e){
-            // createArea.nickname=e.path[0].value;
-            console.log('tag', '')
+            let id=e.srcElement.id;
+            let value=e.path[0].value;
+            if(id==='nickname'){
+                createArea.nickname=value;
+            }
+            else if(id==='content'){
+                createArea.content=value;
+            }
         },
         submit:function(){
             if(!createArea.nickname||!createArea.content){
-                console.log('fall');
+                alert('昵称和留言内容都要写哦')
+            }
+            else{
+                createArea.buttonText="正在发送中";
+                $.post(serverSidePath,{
+                    'nickname':createArea.nickname,
+                    'content':createArea.content,
+                },function(data,sta){
+                    alert('留言成功!!');
+                    location.reload();
+                })
             }
         },
     },
--- serverSide/messages.php
+++ serverSide/messages.php
@@ -1,24 +1,22 @@
 <?php
     header('Access-Control-Allow-Origin:*');
-    if(empty($_POST['nickname'])||empty($_POST['content'])){
-        echo "post";
+    date_default_timezone_set("Asia/Shanghai");
+    function getSqlResult($sql,$con){
+        $responce=$con->query($sql);
+        $result=[];
+        $i=0;
+        while($arr=$responce->fetch_array()){
+            $result[$i]["id"]=$arr["id"];
+            $result[$i]["nickname"]=$arr["nickname"];
+            $result[$i]["content"]=$arr["content"];
+            $result[$i]["time"]=date("Y-m-d H:i",$arr["time"]);
+            $i++;
+        }   
+        return $result;
     }
-    else{
-        function getSqlResult($sql,$con){
-            $responce=$con->query($sql);
-            $result=[];
-            $i=0;
-            while($arr=$responce->fetch_array()){
-                $result[$i]["id"]=$arr["id"];
-                $result[$i]["nickname"]=$arr["nickname"];
-                $result[$i]["content"]=$arr["content"];
-                $result[$i]["time"]=$arr["time"];
-                $i++;
-            }   
-            return $result;
-        }
-        $con=mysqli_connect('localhost','root','root');
-        $con->select_db('messageboard');
+    $con=mysqli_connect('localhost','root','root');
+    $con->select_db('messageboard');
+    if(empty($_POST['nickname'])||empty($_POST['content'])){
         $messages=getSqlResult('select * from messages',$con);
         $responce=[
             'ret' => 200,
@@ -29,5 +27,28 @@
         ];
         $responce=json_encode($responce,JSON_UNESCAPED_UNICODE);
         echo $responce;
+    }
+    else{
+        $nickname=addslashes($_POST['nickname']);
+        $content=addslashes($_POST['content']);
+        $currentTime=time();
+        if(empty($nickname)||empty($content)){
+            $responce=[
+                'ret' => 201,
+                'desc' => 'fall',
+            ];
+            $responce=json_encode($responce,JSON_UNESCAPED_UNICODE);
+            echo $responce;
+        }
+        else{
+            $queryCommand="insert into messages (nickname,content,time) values ('$nickname','$content',$currentTime)";
+            $con->query($queryCommand);
+            $responce=[
+                'ret' => 200,
+                'desc' => 'success',
+            ];
+            $responce=json_encode($responce,JSON_UNESCAPED_UNICODE);
+            echo $responce;
+        }
     }
 ?>
